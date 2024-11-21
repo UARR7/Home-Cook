21034c034836ea5cec8e5398c2a41b7acfe8ca97
Akashe <capt4001@gmail.com>
2024-11-21 15:38:19 +0530

    changes uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://capt4001:JFe7PUrjcs8qj4rR@cluster1.z8n49.mongodb.net/food-del?retryWrites=true&w=majority&appName=Cluster1')
+    await mongoose.connect()
     .then(() => console.log("DB Connected"))
     .catch((err) => console.log(err));
 
